9f6844b01759c8fdd8ba649df5ce231c1f803cb8
gaspatt <matheusgaspar29.if@gmail.com>
2025-04-16 14:18:09 -0300

    .

--- assets/app.js
+++ assets/app.js
@@ -2,27 +2,114 @@ const { createApp, ref } = Vue;
 
 createApp({
   data() {
-    const carrinho = ref(0);
-    const EmEstoque = ref(15);
-    const adicionarCarrinho = () => {
-      carrinho.value += 1;
-      EmEstoque.value -= 1;
+    const personagens = ref([
+      {
+        id: 1,
+        nome: "Homem de Ferro",
+        estoque: 15,
+        image: "./assets/imagens/ironman.png",
+        hoverImage: "./assets/imagens/ironman-hover.png",
+      },
+      {
+        id: 2,
+        nome: "Homem-Aranha",
+        estoque: 8,
+        image: "./assets/imagens/spiderman.png",
+        hoverImage: "./assets/imagens/spiderman-hover.png",
+      },
+      {
+        id: 3,
+        nome: "Thor",
+        estoque: 5,
+        image: "./assets/imagens/thor.png",
+        hoverImage: "./assets/imagens/thor-hover.png",
+      },
+      {
+        id: 4,
+        nome: "Hulk",
+        estoque: 10,
+        image: "./assets/imagens/hulk.png",
+        hoverImage: "./assets/imagens/hulk-hover.png",
+      },
+      {
+        id: 5,
+        nome: "Pantera Negra",
+        estoque: 7,
+        image: "./assets/imagens/blackwidow.png",
+        hoverImage: "./assets/imagens/blackwidow-hover.png",
+      },
+      {
+        id: 6,
+        nome: "Capitão América",
+        estoque: 12,
+        image: "./assets/imagens/captainamerica.png",
+        hoverImage: "./assets/imagens/captainamerica-hover.png",
+      },
+      {
+        id: 7,
+        nome: "Doutor Estranho",
+        estoque: 6,
+        image: "./assets/imagens/doctorstrange.png",
+        hoverImage: "./assets/imagens/doctorstrange-hover.png",
+      },
+      {
+        id: 8,
+        nome: "Falcão",
+        estoque: 9,
+        image: "./assets/imagens/blackpanther.png",
+        hoverImage: "./assets/imagens/blackpanther-hover.png",
+      },
+      {
+        id: 9,
+        nome: "Soldado Invernal",
+        estoque: 4,
+        image: "./assets/imagens/scarletwitch.png",
+        hoverImage: "./assets/imagens/scarletwitch-hover.png",
+      },
+      {
+        id: 10,
+        nome: "Groot",
+        estoque: 11,
+        image: "./assets/imagens/antman.png",
+        hoverImage: "./assets/imagens/antman-hover.png",
+      },
+      {
+        id: 11,
+        nome: "Rocket Raccoon",
+        estoque: 5,
+        image: "./assets/imagens/hawkeye.png",
+        hoverImage: "./assets/imagens/hawkeye-hover.png",
+      },
+      {
+        id: 12,
+        nome: "Gavião Arqueiro",
+        estoque: 3,
+        image: "./assets/imagens/vision.png",
+        hoverImage: "./assets/imagens/vision-hover.png",
+      },
+      {
+        id: 13,
+        nome: "PARCERIA IMPERDIVEL - Coringa",
+        estoque: 1,
+        image: "./assets/imagens/coringa.png",
+        hoverImage: "./assets/imagens/coringa-hover.png",
+      },
+    ]);
+    const carrinho = ref([]);
+
+    const adicionarCarrinho = (personagem) => {
+      if (personagem.estoque > 0) {
+        carrinho.value.push(personagem);
+        personagem.estoque -= 1;
+      }
     };
-    const image = ref("./assets/imagens/ironman.png");
-    const mudarImagem = (varianteImage) => {
-      image.value = varianteImage;
+
+    const mudarImagem = (personagem, hover) => {
+      personagem.image = hover ? personagem.hoverImage : personagem.image;
     };
+
     return {
-      personagem_nome: ref("Iron Man"),
-      universo: "Marvel",
-      image,
-      EmEstoque,
-      detalhes: ["Armadura de Ferro", "Tecnologia Stark", "Vingador"],
-      variantes: [
-        { id: 1, cor: "red", image: "assets/imagens/ironman.png" },
-        { id: 2, cor: "gold", image: "assets/imagens/ironman-gold.png" },
-        { id: 3, cor: "black", image: "assets/imagens/ironman-black.png" },
-      ],
+      personagens,
       carrinho,
       adicionarCarrinho,
       mudarImagem,
